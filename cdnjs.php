d9096a7a4a8f5a87bd0a5b59a9dc4c3adf1a01e4
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-06-18 02:16:05 +0600

    Revert cdnjs.php to original (remove Tailwind CDN)

--- cdnjs.php
+++ cdnjs.php
@@ -3,11 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
 
-<script src="https://cdn.tailwindcss.com"></script>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
-
 <link rel="stylesheet" 
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" 
   integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" 
@@ -17,6 +12,9 @@
 <link rel="stylesheet" href="../cdn.jsdelivr.net/npm/%40fancyapps/fancybox%403.5.6/dist/jquery.fancybox.min.css" />
 <link rel="stylesheet" href="../cdn.jsdelivr.net/npm/cooltipz-css/cooltipz.min.css">
 <link rel="stylesheet" href="../cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+
+<link href="../cdn.jsdelivr.net/npm/suneditor%40latest/dist/css/suneditor.min.css" rel="stylesheet">
+<script src="../cdn.jsdelivr.net/npm/suneditor%40latest/dist/suneditor.min.js"></script>
 
 <link rel="stylesheet" href="styles/styles.css" type="text/css" />
 <link rel="stylesheet" href="styles/custom.css" type="text/css" />
