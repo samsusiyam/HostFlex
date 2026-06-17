c49577724c5d81ae4e57aecfc3252d75a1ad0712
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-06-18 00:51:49 +0600

    Set GitHub repo for update system

--- config/version.php
+++ config/version.php
@@ -2,4 +2,4 @@
 define('APP_VERSION', '1.0.0');
 define('APP_VERSION_DATE', '2026-06-18');
 define('APP_NAME', 'HostNibo');
-define('GITHUB_REPO', ''); // e.g. 'username/repo'
+define('GITHUB_REPO', 'samsusiyam/HostFlex');
