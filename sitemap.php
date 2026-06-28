d1df2abff3ba0fab43bbf7fa5b26a0134b0389b7
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-06-28 21:35:06 +0600

    fix: make sitemap error-proof - skip failed queries instead of crashing with 500

--- sitemap.php
+++ sitemap.php
@@ -9,52 +9,41 @@ define('SITE_URL', $protocol . '://' . $host . $dir . '/');
 
 header('Content-Type: application/xml; charset=utf-8');
 
-$blogPosts = $conn->query("SELECT slug, updated_at FROM blog_posts WHERE status = 1");
-$pages = $conn->query("SELECT slug FROM pages WHERE status = 1");
-$categories = $conn->query("SELECT slug FROM blog_categories WHERE status = 1");
+$urls = [
+    ['loc' => SITE_URL . 'index.php', 'priority' => '1.0'],
+    ['loc' => SITE_URL . 'blogs.php', 'changefreq' => 'weekly', 'priority' => '0.8'],
+    ['loc' => SITE_URL . 'contact.php', 'changefreq' => 'monthly', 'priority' => '0.5'],
+    ['loc' => SITE_URL . 'offers.php', 'changefreq' => 'monthly', 'priority' => '0.5'],
+];
+
+if ($conn) {
+    $r = @$conn->query("SELECT slug, updated_at FROM blog_posts WHERE status = 1");
+    if ($r) { while ($blog = $r->fetch_assoc()) {
+        $urls[] = ['loc' => SITE_URL . 'blog/' . $blog['slug'], 'lastmod' => date('c', strtotime($blog['updated_at'])), 'changefreq' => 'weekly', 'priority' => '0.6'];
+    }}
+    $r = @$conn->query("SELECT slug FROM pages WHERE status = 1");
+    if ($r) { while ($page = $r->fetch_assoc()) {
+        $urls[] = ['loc' => SITE_URL . 'page/' . $page['slug'], 'changefreq' => 'monthly', 'priority' => '0.5'];
+    }}
+    $r = @$conn->query("SELECT slug FROM blog_categories WHERE status = 1");
+    if ($r) { while ($cat = $r->fetch_assoc()) {
+        $urls[] = ['loc' => SITE_URL . 'category/' . $cat['slug'], 'changefreq' => 'monthly', 'priority' => '0.5'];
+    }}
+}
 
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<?php foreach ($urls as $u): ?>
     <url>
-        <loc><?= SITE_URL ?>index.php</loc>
-        <priority>1.0</priority>
+        <loc><?= htmlspecialchars($u['loc']) ?></loc>
+<?php if (!empty($u['lastmod'])): ?>
+        <lastmod><?= $u['lastmod'] ?></lastmod>
+<?php endif; ?>
+<?php if (!empty($u['changefreq'])): ?>
+        <changefreq><?= $u['changefreq'] ?></changefreq>
+<?php endif; ?>
+        <priority><?= $u['priority'] ?></priority>
     </url>
-    <url>
-        <loc><?= SITE_URL ?>blogs.php</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc><?= SITE_URL ?>contact.php</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-    <url>
-        <loc><?= SITE_URL ?>offers.php</loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-<?php while ($blog = $blogPosts->fetch_assoc()): ?>
-    <url>
-        <loc><?= SITE_URL ?>blog/<?= htmlspecialchars($blog['slug']) ?></loc>
-        <lastmod><?= date('c', strtotime($blog['updated_at'])) ?></lastmod>
-        <changefreq>weekly</changefreq>
-        <priority>0.6</priority>
-    </url>
-<?php endwhile; ?>
-<?php while ($page = $pages->fetch_assoc()): ?>
-    <url>
-        <loc><?= SITE_URL ?>page/<?= htmlspecialchars($page['slug']) ?></loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-<?php endwhile; ?>
-<?php while ($cat = $categories->fetch_assoc()): ?>
-    <url>
-        <loc><?= SITE_URL ?>category/<?= htmlspecialchars($cat['slug']) ?></loc>
-        <changefreq>monthly</changefreq>
-        <priority>0.5</priority>
-    </url>
-<?php endwhile; ?>
+<?php endforeach; ?>
 </urlset>
