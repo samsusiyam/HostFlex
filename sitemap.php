70389db586e7f7576f08af0da7466a5d7f69864e
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-06-28 23:33:33 +0600

    fix: rewrite sitemap - remove ob_start, use string building to avoid session/headers conflicts causing 500

--- sitemap.php
+++ sitemap.php
@@ -2,15 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
 ini_set('log_errors', 1);
-ob_start();
+
 require 'config/database.php';
 require 'includes/functions.php';
-
-if (headers_sent()) {
-    ob_end_flush();
-    exit;
-}
-ob_end_clean();
 
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
 $host = $_SERVER['HTTP_HOST'];
@@ -19,41 +13,41 @@ define('SITE_URL', $protocol . '://' . $host . $dir . '/');
 
 header('Content-Type: application/xml; charset=utf-8');
 
-$urls = [
-    ['loc' => SITE_URL . 'index.php', 'priority' => '1.0'],
-    ['loc' => SITE_URL . 'blogs.php', 'changefreq' => 'weekly', 'priority' => '0.8'],
-    ['loc' => SITE_URL . 'contact.php', 'changefreq' => 'monthly', 'priority' => '0.5'],
-    ['loc' => SITE_URL . 'offers.php', 'changefreq' => 'monthly', 'priority' => '0.5'],
-];
+$xml  = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
-if ($conn) {
-    $r = @$conn->query("SELECT slug, updated_at FROM blog_posts WHERE status = 1");
-    if ($r) { while ($blog = $r->fetch_assoc()) {
-        $urls[] = ['loc' => SITE_URL . 'blog/' . $blog['slug'], 'lastmod' => date('c', strtotime($blog['updated_at'])), 'changefreq' => 'weekly', 'priority' => '0.6'];
-    }}
-    $r = @$conn->query("SELECT slug FROM pages WHERE status = 1");
-    if ($r) { while ($page = $r->fetch_assoc()) {
-        $urls[] = ['loc' => SITE_URL . 'page/' . $page['slug'], 'changefreq' => 'monthly', 'priority' => '0.5'];
-    }}
-    $r = @$conn->query("SELECT slug FROM blog_categories WHERE status = 1");
-    if ($r) { while ($cat = $r->fetch_assoc()) {
-        $urls[] = ['loc' => SITE_URL . 'category/' . $cat['slug'], 'changefreq' => 'monthly', 'priority' => '0.5'];
-    }}
+function sm_url($loc, $changefreq = '', $priority = '0.5', $lastmod = '') {
+    $xml  = '    <url>' . "\n";
+    $xml .= '        <loc>' . htmlspecialchars($loc) . '</loc>' . "\n";
+    if ($lastmod) $xml .= '        <lastmod>' . $lastmod . '</lastmod>' . "\n";
+    if ($changefreq) $xml .= '        <changefreq>' . $changefreq . '</changefreq>' . "\n";
+    $xml .= '        <priority>' . $priority . '</priority>' . "\n";
+    $xml .= '    </url>' . "\n";
+    return $xml;
 }
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-<?php foreach ($urls as $u): ?>
-    <url>
-        <loc><?= htmlspecialchars($u['loc']) ?></loc>
-<?php if (!empty($u['lastmod'])): ?>
-        <lastmod><?= $u['lastmod'] ?></lastmod>
-<?php endif; ?>
-<?php if (!empty($u['changefreq'])): ?>
-        <changefreq><?= $u['changefreq'] ?></changefreq>
-<?php endif; ?>
-        <priority><?= $u['priority'] ?></priority>
-    </url>
-<?php endforeach; ?>
-</urlset>
+$xml .= sm_url(SITE_URL . 'index.php', '', '1.0');
+$xml .= sm_url(SITE_URL . 'blogs.php', 'weekly', '0.8');
+$xml .= sm_url(SITE_URL . 'contact.php', 'monthly', '0.5');
+$xml .= sm_url(SITE_URL . 'offers.php', 'monthly', '0.5');
+
+if ($conn) {
+    $tables = [
+        ['table' => 'blog_posts', 'url' => 'blog/', 'changefreq' => 'weekly', 'priority' => '0.6', 'has_date' => true],
+        ['table' => 'pages', 'url' => 'page/', 'changefreq' => 'monthly', 'priority' => '0.5', 'has_date' => false],
+        ['table' => 'blog_categories', 'url' => 'category/', 'changefreq' => 'monthly', 'priority' => '0.5', 'has_date' => false],
+    ];
+    foreach ($tables as $t) {
+        $cols = 'slug';
+        if ($t['has_date']) $cols .= ', updated_at';
+        $r = @$conn->query("SELECT $cols FROM `{$t['table']}` WHERE status = 1");
+        if (!$r) continue;
+        while ($row = $r->fetch_assoc()) {
+            $lastmod = ($t['has_date'] && !empty($row['updated_at'])) ? date('c', strtotime($row['updated_at'])) : '';
+            $xml .= sm_url(SITE_URL . $t['url'] . $row['slug'], $t['changefreq'], $t['priority'], $lastmod);
+        }
+    }
+}
+
+$xml .= '</urlset>';
+echo $xml;
