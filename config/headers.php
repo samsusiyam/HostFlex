d32b37a093e3ad5bb7d81c0de5819175c075718a
MD Samsuzzaman Siyam <samsusiyam@gmail.com>
2026-07-01 01:30:38 +0600

    Fix CSP to allow chatwoot.siyam.eu.cc in script-src and connect-src

--- config/headers.php
+++ config/headers.php
@@ -22,11 +22,11 @@ if (empty(headers_sent())) {
 
     $csp = implode('; ', [
         "default-src 'self'",
-        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tailwindcss.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://embed.tawk.to",
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.tailwindcss.com https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://embed.tawk.to https://chatwoot.siyam.eu.cc",
         "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://cdn.jsdelivr.net https://fonts.googleapis.com https://cdn.tailwindcss.com",
         "img-src 'self' data: https:",
         "font-src 'self' https://cdnjs.cloudflare.com https://fonts.gstatic.com https://cdn.jsdelivr.net",
-        "connect-src 'self' https://embed.tawk.to wss://embed.tawk.to",
+        "connect-src 'self' https://embed.tawk.to wss://embed.tawk.to https://chatwoot.siyam.eu.cc wss://chatwoot.siyam.eu.cc",
         "frame-src https://embed.tawk.to https://www.youtube.com https://www.google.com",
         "object-src 'none'",
         "base-uri 'self'",
